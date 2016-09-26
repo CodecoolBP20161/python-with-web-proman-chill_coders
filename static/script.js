cc3d2a12f28016b2499ead7ad4249bcd412895dd
dsztanko <szt.dori@gmail.com>
2016-09-26 22:30:26 +0200

    storage in local scope

--- static/script.js
+++ static/script.js
@@ -1,7 +1,19 @@
+var list_of_boards = [];
+
+function Board (title) {
+    this.title = title
+}
+
 $(document).ready(function() {
     $('button').click(function() {
         var toAdd = $("input[name=board]").val();
         $('div').append("<p>" + toAdd + "</p>");
+        var board = new Board(toAdd);
+        list_of_boards.push(board);
+        console.log(list_of_boards);
+        for (var i = 0; i < list_of_boards.length; i++) {
+            console.log(list_of_boards[i].title);
+        }
     $('p').hover(
         function(){
             $(this).addClass('active')
@@ -11,3 +23,6 @@ $(document).ready(function() {
         });
     });
 });
+
+
+
