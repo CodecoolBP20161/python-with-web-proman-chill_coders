cadb7bcadb23b13cc3f7722936582eddd30e5922
dsztanko <szt.dori@gmail.com>
2016-09-27 00:13:14 +0200

    localStorage stores objects

--- static/script.js
+++ static/script.js
@@ -1,27 +1,25 @@
-var list_of_boards = [];
-
 function Board (title) {
     this.title = title
 }
 
+
 $(document).ready(function() {
-    var fruits = ['barack', 'maracuja']
-    for (var i = 0; i < fruits.length; i++) {
-        $('div').append("<p>" + fruits[i] + "</p>");
+    // localStorage.clear();
+    console.log(localStorage);
+    for (var i = 0; i < localStorage.length; i++) {
+        console.log(localStorage.getItem('board' + i));
+        var board = JSON.parse(localStorage.getItem('board' + i));
+        console.log(board);
+        $('div').append("<p>" + board.title + "</p>");
     }
+
     $('button').click(function () {
         var toAdd = $("input[name=board]").val();
         $('div').append("<p>" + toAdd + "</p>");
         var board = new Board(toAdd);
-        list_of_boards.push(board);
-        console.log(list_of_boards);
-        for (var i = 0; i < list_of_boards.length; i++) {
-            console.log(list_of_boards[i].title);
-            localStorage.setItem('board' + i, JSON.stringify(
-                {title: list_of_boards[i].title}));
-            console.log(JSON.parse(localStorage.getItem('board' + i)))
-        }
+        localStorage.setItem('board' + i, JSON.stringify(board));
     });
+
     $('p').hover(
         function () {
             $(this).addClass('active');
