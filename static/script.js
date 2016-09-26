3eabb2e296355d3e79a7a7e54a0c323649bb72aa
dsztanko <szt.dori@gmail.com>
2016-09-26 23:05:00 +0200

    loading elements without local storage

--- static/script.js
+++ static/script.js
@@ -5,7 +5,11 @@ function Board (title) {
 }
 
 $(document).ready(function() {
-    $('button').click(function() {
+    var fruits = ['barack', 'maracuja']
+    for (var i = 0; i < fruits.length; i++) {
+        $('div').append("<p>" + fruits[i] + "</p>");
+    }
+    $('button').click(function () {
         var toAdd = $("input[name=board]").val();
         $('div').append("<p>" + toAdd + "</p>");
         var board = new Board(toAdd);
@@ -13,16 +17,20 @@ $(document).ready(function() {
         console.log(list_of_boards);
         for (var i = 0; i < list_of_boards.length; i++) {
             console.log(list_of_boards[i].title);
+            localStorage.setItem('board' + i, JSON.stringify(
+                {title: list_of_boards[i].title}));
+            console.log(JSON.parse(localStorage.getItem('board' + i)))
         }
+    });
     $('p').hover(
-        function(){
-            $(this).addClass('active')
+        function () {
+            $(this).addClass('active');
         },
-        function(){
+        function () {
             $(this).removeClass('active');
         });
-    });
 });
+
 
 
 
