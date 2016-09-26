189ee2a21ba777f52bbe135d422bc0987f80adc4
dsztanko <szt.dori@gmail.com>
2016-09-26 16:55:06 +0200

    adding elements to page through input field

--- static/script.js
+++ static/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('button').click(function() {
         var toAdd = $("input[name=message]").val();
-        $('#messages').append("<p>"+toAdd+"</p>");
+        $('#messages').append("<p>" + toAdd + "</p>");
     });
 });
