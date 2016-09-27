ad1adba1f21e0d7952369dfc008f65c47f45ce98
dsztanko <szt.dori@gmail.com>
2016-09-27 13:32:05 +0200

    basic sample with js

---
+++ static/script.js
@@ -0,0 +1,26 @@
+function Board (title) {
+    this.title = title
+}
+
+
+$(document).ready(function() {
+    localStorage.clear();
+    // console.log(localStorage);
+    for (var i = 0; i < localStorage.length; i++) {
+        console.log(localStorage.getItem('board' + i));
+        var board = JSON.parse(localStorage.getItem('board' + i));
+        console.log(board);
+        $('div').append("<p>" + board.title + "</p>");
+    }
+
+    $('button').click(function () {
+        var toAdd = $("input[name=board]").val();
+        $('div').append("<p>" + toAdd + "</p>");
+        var board = new Board(toAdd);
+        localStorage.setItem('board' + i, JSON.stringify(board));
+    });
+});
+
+
+
+
