5d4af53aa31ed8ee489e80c3e19d0a5966137cae
andras <doraandras14@gmail.com>
2016-09-27 11:22:07 +0200

    fixing changes

--- proman.py
+++ proman.py
@@ -3,9 +3,9 @@ from flask import Flask,render_template
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-@app.route('/', methods= ['GET'])
+@app.route('/')
 def main():
-    return render_template("empty.html")
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
