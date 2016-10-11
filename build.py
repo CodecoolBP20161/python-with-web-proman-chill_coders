6a81f892e5fe22ee057546e979130e834f48f3ad
dsztanko <szt.dori@gmail.com>
2016-10-11 18:19:58 +0200

    models done, flask and jsonifying in progress

---
+++ build.py
@@ -0,0 +1,12 @@
+from models import *
+
+
+db.drop_tables([Board, Card, Meta])
+db.connect()
+db.create_tables([Board, Card, Meta], safe=True)
+Board.create(title='alma', color='tile-green')
+Board.create(title='körte', color='tile-green')
+Board.create(title='szilva', color='tile-green')
+Board.create(title='banán', color='tile-green')
+Card.create(title='alma1', color='tile-green', board=1)
+Meta.create(page_state='board_level')
