cd71a2dce11bc73b7bdd739b84650700ed44969d
dsztanko <szt.dori@gmail.com>
2016-10-14 09:27:34 +0200

    bug fixed

--- static/control/script.js
+++ static/control/script.js
@@ -7,7 +7,7 @@
 // for Implementation1
 var storage = new State(new LocalStorageManager('listOfBoards'));
 // for Implementation2
-storage.changeState(new DatabaseStorageManager());
+// storage.changeState(new DatabaseStorageManager());
 
 
 // **** MAIN ****
